b787048e7fc92146ccd1af672fbe7135563358b8
HarshSagar11 <harshsagarkumar1111@gmail.com>
2024-07-11 14:27:02 +0530

    Week 2 beginning

---
+++ week2Learning/src/main/java/com/spingboot/week2/learning/week2Learning/Week2LearningApplication.java
@@ -0,0 +1,13 @@
+package com.spingboot.week2.learning.week2Learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2LearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week2LearningApplication.class, args);
+	}
+
+}
---
+++ week2Learning/src/test/java/com/spingboot/week2/learning/week2Learning/Week2LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spingboot.week2.learning.week2Learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
