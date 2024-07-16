fe48b8ee00891f8c3cfd76d8be0112437e7a7569
HarshSagar11 <harshsagarkumar1111@gmail.com>
2024-07-17 00:00:05 +0530

    week 2 home work start

---
+++ Week2HomeWork/src/test/java/com/example/week2HW/Week2HomeWork/Week2HomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week2HW.Week2HomeWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2HomeWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
