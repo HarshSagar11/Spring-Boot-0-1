6f6764968666f54904226f9d13cba564f293c651
HarshSagar11 <harshsagarkumar1111@gmail.com>
2024-07-21 03:01:02 +0530

    week 3 added queries for spring DataJPA

---
+++ Week3/src/main/java/com/week3/JPATutorial/Week3/repositories/ProductRepository.java
@@ -0,0 +1,23 @@
+package com.week3.JPATutorial.Week3.repositories;
+
+import com.week3.JPATutorial.Week3.entities.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<ProductEntity,Long> {
+    List<ProductEntity> findByTitle(String pepsi);
+
+    List<ProductEntity> findByCreatedAtAfter (LocalDateTime after);
+    List<ProductEntity> findByQuantityGreaterThanOrPriceLessThan (int quantity, BigDecimal price);
+    List<ProductEntity> findByTitleLike (String title);
+    List<ProductEntity> findByTitleContainingIgnoreCase(String title);
+//    Optional<ProductEntity> findByTitleAndPrice (String title, BigDecimal price);
+    @Query("select e from ProductEntity e where e.title=:title and e.price=:price")
+//    @Query("select e.title from ProductEntity e where e.title=?1 and e.price=?2") // other way for parameter
+    Optional <ProductEntity> findByTitleAndPrice (String title, BigDecimal price);
+}
--- Week3/src/test/java/com/week3/JPATutorial/Week3/Week3ApplicationTests.java
+++ Week3/src/test/java/com/week3/JPATutorial/Week3/Week3ApplicationTests.java
@@ -1,13 +1,39 @@
 package com.week3.JPATutorial.Week3;
 
+import com.week3.JPATutorial.Week3.entities.ProductEntity;
+import com.week3.JPATutorial.Week3.repositories.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @SpringBootTest
 class Week3ApplicationTests {
 
+	@Autowired
+	ProductRepository productRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testRepository() {
+		ProductEntity productEntity = ProductEntity.builder()
+				.sku("nestle234")
+				.title("Nestle Chocolate")
+				.price (BigDecimal.valueOf(123.45))
+				.quantity (12)
+				.build();
+		ProductEntity savedProductEntity = productRepository.save(productEntity);
+		System.out.println(savedProductEntity);
+	}
+	@Test
+	void getRepository() {
+		List<ProductEntity> entities = productRepository.findByTitle("Pepsi");
+		System.out.println(entities);
 	}
 
 }
