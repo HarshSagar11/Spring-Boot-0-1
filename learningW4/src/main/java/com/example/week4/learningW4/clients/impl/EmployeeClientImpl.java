88cab62358c255498d9293956bab63d3fa3fd980
HarshSagar11 <harshsagarkumar1111@gmail.com>
2024-08-06 21:47:07 +0530

    week 4 continue

--- learningW4/src/main/java/com/example/week4/learningW4/clients/impl/EmployeeClientImpl.java
+++ learningW4/src/main/java/com/example/week4/learningW4/clients/impl/EmployeeClientImpl.java
@@ -5,6 +5,8 @@ import com.example.week4.learningW4.clients.EmployeeClient;
 import com.example.week4.learningW4.dto.EmployeesDTO;
 import com.example.week4.learningW4.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
@@ -18,19 +20,34 @@ import java.util.List;
 public class EmployeeClientImpl implements EmployeeClient {
 
     private final RestClient restClient;
+    Logger log = LoggerFactory.getLogger(EmployeeClientImpl.class);
 
     @Override
     public List<EmployeesDTO> getAllEmployees() {
+
+//        log.error("error log");
+//        log.warn("warn log");
+//        log.info("info log");
+//        // below 2 log can't be seen as it is it needs to be enabled by log levels
+//        log.debug("debug log");
+//        log.trace("trace log");
+    log.trace("Trying to retrieve all the employee from getAllEmployee");
         try{
             ApiResponse<List<EmployeesDTO>> employeesDTOList = restClient.get()
                     .uri("employees")
                     .retrieve()
+                    .onStatus(HttpStatusCode::is4xxClientError ,(req,resp)->{
+                        log.error(new String(resp.getBody().readAllBytes()));
+                        throw new ResourceNotFoundException("could not create the Employee");
+                    })
                     .body(new ParameterizedTypeReference<>(){
-
                     });
+            log.info("successfully retrieved all the employee");
+            log.trace("Retrieved employee list in get all Employee : {}", employeesDTOList.getData());
             return employeesDTOList.getData();
         }
         catch (Exception e){
+            log.error("Exception occurred in getAllEmployee",e); // this will print all the stack trace
             throw new RuntimeException("some error in rest client");
         }
     }
@@ -58,7 +75,7 @@ public class EmployeeClientImpl implements EmployeeClient {
                     .body(employee)
                     .retrieve()
                     .onStatus(HttpStatusCode::is4xxClientError ,(req,resp)->{
-                        System.out.println(new String(resp.getBody().readAllBytes()));
+                        log.error(new String(resp.getBody().readAllBytes()));
                         throw new ResourceNotFoundException("could not create the Employee");
                     })
                     .body(new ParameterizedTypeReference<>(){});
