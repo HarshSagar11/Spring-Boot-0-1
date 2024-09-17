d2b55695b2be807152fa48adf3cb85d40e987d62
Harsh windows <harshsagarkumar1111@gmail.com>
2024-09-17 21:04:43 +0530

    week 7 testing

--- TestingApp/src/test/java/week7/testingApp/TestingApp/TestingAppApplicationTests.java
+++ TestingApp/src/test/java/week7/testingApp/TestingApp/TestingAppApplicationTests.java
@@ -1,13 +1,40 @@
 package week7.testingApp.TestingApp;
 
-import org.junit.jupiter.api.Test;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.data.Offset;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+//@SpringBootTest // this annotation run whole spring application
+@Slf4j
 class TestingAppApplicationTests {
 
-	@Test
-	void contextLoads() {
+//	@BeforeAll // run before all test case (method must be static)
+// 	@AfterAll // run after all test case (method must be static)
+//	Assertions.assertEquals compare 2 things, 2 inputs or 1 expected value and 1 return value of fn
+
+	@BeforeEach //before every test case
+	void setup(){
+		log.info("Starting the method, setting up config");
+	}
+
+	@AfterEach //before every test case
+	void finishingUp(){
+		log.info("Ending the method");
+	}
+
+	@Test // mark a method as test method
+	void testNumber1() {
+		assertThat(1).isEqualTo(1).isCloseTo(2, Offset.offset(1));
+		log.info("test 1 is run");
+	}
+
+	@Test // mark a method as test method
+	void testNumber2() {
+		log.info("test 2 is run");
 	}
 
 }
